d279488c881ae3a31bfcf408af9f4e5a8006ebc2
Kenerry Serain <38140596+kenerry-serain@users.noreply.github.com>
2025-04-04 08:25:49 -0300

    Uploading

--- terraform/main-stack/main.tf
+++ terraform/main-stack/main.tf
@@ -25,6 +25,11 @@ provider "aws" {
   }
 }
 
+provider "aws" {
+  region = "us-east-1"
+  alias  = "virginia"
+}
+
 provider "kubernetes" {
   host                   = aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
